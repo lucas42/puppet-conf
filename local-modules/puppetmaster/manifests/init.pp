07dc6ce52fe4f9092e6a4634384aa5fee52b9965
Luke Blaney <git@lukeblaney.co.uk>
2014-05-11 12:14:14 +0200

    Allow me to edit puppet config without being root

--- local-modules/puppetmaster/manifests/init.pp
+++ local-modules/puppetmaster/manifests/init.pp
@@ -7,4 +7,13 @@ class puppetmaster {
 		ensure => "running",
 		require => Package["puppetmaster"],
 	}
+	file { "/etc/puppet":
+		ensure => "directory",
+		owner => lucas,
+		group => root,
+		mode => 644,
+		require => [User["lucas"], Package["puppetmaster"]],
+		recurse => true,
+	}
+	
 }
