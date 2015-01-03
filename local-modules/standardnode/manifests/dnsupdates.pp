ea427fdaf858d3ce33dd4a5b2db117d9a2089558
Luke Blaney <git@lukeblaney.co.uk>
2015-01-03 13:09:27 +0000

    don't update DNS for development machines

--- local-modules/standardnode/manifests/dnsupdates.pp
+++ local-modules/standardnode/manifests/dnsupdates.pp
@@ -2,11 +2,17 @@ class standardnode::dnsupdates (
 	$dnsserver,
 	$authkey,
 ){
-	$minoffset = fqdn_rand(10)
-	cron { 'updatedns':
-		command => "curl -X PUT 'http://${dnsserver}/servers/${::hostname}' -H 'Authorization: Key ${authkey}'",
-		user => "root",
-		minute => "$minoffset-59/10",
-		ensure => present,
+	if (!$::development) {
+		$minoffset = fqdn_rand(10)
+		cron { 'updatedns':
+			command => "curl -X PUT 'http://${dnsserver}/servers/${::hostname}' -H 'Authorization: Key ${authkey}'",
+			user => "root",
+			minute => "$minoffset-59/10",
+			ensure => present,
+		}
+	} else {
+		cron { 'updatedns':
+			ensure => absent,
+		}
 	}
 }
