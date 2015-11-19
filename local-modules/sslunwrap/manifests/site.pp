0c79d92b51b5d610c08c2ea9e6fe48906945cb9f
Luke Blaney <git@lukeblaney.co.uk>
2015-11-19 00:33:50 +0000

    run letsencrypt script for domains which need it (very hacky)

--- local-modules/sslunwrap/manifests/site.pp
+++ local-modules/sslunwrap/manifests/site.pp
@@ -5,5 +5,16 @@ define sslunwrap::site () {
 		content => template("${module_name}/site.erb"),
 		notify  => Service['nginx'],
 	}
+
+	# Assumes that letsencript-auto has already been install
+	# TODO: automate installation of said script
+	# Need to stop nginx first as letsencrypt uses port 443 for verification
+	# TODO: use nginx plugin for letsencript once it's stable
+	exec { "/usr/sbin/service nginx stop && /etc/letsencrypt/letsencrypt-auto certonly --standalone --standalone-supported-challenges tls-sni-01 --server https://acme-v01.api.letsencrypt.org/directory --agree-dev-preview --agree-tos -d ${domain}":
+		creates => "/etc/letsencrypt/live/${domain}",
+
+		# Not sure whether nginx needs bounced when certs change, but do it just in case.
+		notify  => Service['nginx'],
+	}
 }
 
