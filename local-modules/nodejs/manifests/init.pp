87bf853df9955830d17c49d80c9686095d1d45d6
Luke Blaney <git@lukeblaney.co.uk>
2016-01-16 03:15:02 +0000

    alias node command because Debian likes to be different

--- local-modules/nodejs/manifests/init.pp
+++ local-modules/nodejs/manifests/init.pp
@@ -2,4 +2,9 @@ class nodejs {
 	package { ["nodejs", "npm"]:
 		ensure => installed,
 	}
+
+	# Debian calls node a different name than anything expecting to run it.
+	file { '/usr/bin/node':
+		ensure => '/usr/bin/nodejs'
+	}
 }
