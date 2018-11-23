8de10f67c4e7bd4fcddbb4eebe057fe5db342dda
Luke Blaney <git@lukeblaney.co.uk>
2018-11-23 22:37:48 +0000

    start nginx even if letsencrypt script barfs all over the place

--- local-modules/sslunwrap/manifests/site.pp
+++ local-modules/sslunwrap/manifests/site.pp
@@ -10,7 +10,7 @@ define sslunwrap::site () {
 	# TODO: automate installation of said script
 	# Need to stop nginx first as letsencrypt uses port 443 for verification
 	# TODO: use nginx plugin for letsencript once it's stable
-	exec { "/usr/sbin/service nginx stop && /etc/letsencrypt/letsencrypt-auto certonly --standalone --standalone-supported-challenges tls-sni-01 --server https://acme-v01.api.letsencrypt.org/directory --agree-dev-preview --agree-tos -d ${domain} && /usr/sbin/service nginx start":
+	exec { "/usr/sbin/service nginx stop; /etc/letsencrypt/letsencrypt-auto certonly --standalone --standalone-supported-challenges tls-sni-01 --server https://acme-v01.api.letsencrypt.org/directory --agree-dev-preview --agree-tos -d ${domain}; /usr/sbin/service nginx start":
 	
 		# Only run if the cert directory doesn't exist, or the cert will expire in the next 25 days
 		unless    => "/bin/bash -c 'if [ ! -d \"/etc/letsencrypt/live/${domain}\" ] || [[ `/etc/nginx/checkexpiry.sh ${domain}`  -lt 2160000 ]]; then false; fi'",
